23bb2100194cb2bfac3c2f4c2720be08f42df239
LeandroMel0 <lesanto175@gmail.com>
2025-07-25 14:47:19 -0300

    Commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
-      maxAge: process.env.MAXAGE// 1h
+      maxAge: parseInt(process.env.MAXAGE) || 360000 // 1h
     }
   })
 );
